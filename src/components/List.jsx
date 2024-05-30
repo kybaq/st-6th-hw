642699dd41d2f9aad10bed7f23dd19499fc19917
kybaq <flfsk12@gmail.com>
2024-05-30 14:35:58 +0900

    feat: memo() 사용해 컴포넌트 캐싱)

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,5 +1,8 @@
+import { memo } from "react";
+
 // TODO: input 타이핑할 때 리스트 컴포넌트가 불필요하게 렌더링되지 않게 해보세요.
-const List = ({ items }) => {
+// memo: 컴포넌트의 props 가 변경되지 않은 경우 리렌더링을 건너뛸 수 있다.
+const List = memo(({ items }) => {
   console.log("List component rendered");
   return (
     <ul>
@@ -8,6 +11,6 @@ const List = ({ items }) => {
       ))}
     </ul>
   );
-};
+});
 
 export default List;
